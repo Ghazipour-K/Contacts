fada918cb017b3000e408f2aa7af034615d0b65b
IT <IT@IT-PC>
2022-07-15 16:57:15 +0430

    First usable version

--- Contacts/Main.Designer.cs
+++ Contacts/Main.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ContactItemsGoupBox = new System.Windows.Forms.GroupBox();
             this.MobileTextBox = new System.Windows.Forms.TextBox();
             this.LastNameTextBox = new System.Windows.Forms.TextBox();
             this.FirstNameTextBox = new System.Windows.Forms.TextBox();
@@ -37,37 +37,32 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.SaveButton = new System.Windows.Forms.Button();
-            this.NewButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
             this.DeleteButton = new System.Windows.Forms.Button();
-            this.LastRecordButton = new System.Windows.Forms.Button();
-            this.NextRecordButton = new System.Windows.Forms.Button();
-            this.PreviousRecordButton = new System.Windows.Forms.Button();
-            this.FirstRecordButton = new System.Windows.Forms.Button();
             this.CriteriaTextBox = new System.Windows.Forms.TextBox();
-            this.FieldToSearchComboBox = new System.Windows.Forms.ComboBox();
             this.ContactsDataView = new System.Windows.Forms.DataGridView();
             this.tblContactBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.groupBox1.SuspendLayout();
+            this.ContactItemsGoupBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ContactsDataView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tblContactBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // ContactItemsGoupBox
             // 
-            this.groupBox1.Controls.Add(this.MobileTextBox);
-            this.groupBox1.Controls.Add(this.LastNameTextBox);
-            this.groupBox1.Controls.Add(this.FirstNameTextBox);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.groupBox1.Size = new System.Drawing.Size(599, 130);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "مشخصات";
+            this.ContactItemsGoupBox.Controls.Add(this.MobileTextBox);
+            this.ContactItemsGoupBox.Controls.Add(this.LastNameTextBox);
+            this.ContactItemsGoupBox.Controls.Add(this.FirstNameTextBox);
+            this.ContactItemsGoupBox.Controls.Add(this.label3);
+            this.ContactItemsGoupBox.Controls.Add(this.label2);
+            this.ContactItemsGoupBox.Controls.Add(this.label1);
+            this.ContactItemsGoupBox.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContactItemsGoupBox.Location = new System.Drawing.Point(12, 12);
+            this.ContactItemsGoupBox.Name = "ContactItemsGoupBox";
+            this.ContactItemsGoupBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ContactItemsGoupBox.Size = new System.Drawing.Size(599, 130);
+            this.ContactItemsGoupBox.TabIndex = 0;
+            this.ContactItemsGoupBox.TabStop = false;
+            this.ContactItemsGoupBox.Text = "مشخصات";
             // 
             // MobileTextBox
             // 
@@ -76,7 +71,7 @@
             this.MobileTextBox.Name = "MobileTextBox";
             this.MobileTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.MobileTextBox.Size = new System.Drawing.Size(170, 21);
-            this.MobileTextBox.TabIndex = 1;
+            this.MobileTextBox.TabIndex = 2;
             // 
             // LastNameTextBox
             // 
@@ -94,7 +89,7 @@
             this.FirstNameTextBox.Name = "FirstNameTextBox";
             this.FirstNameTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.FirstNameTextBox.Size = new System.Drawing.Size(170, 21);
-            this.FirstNameTextBox.TabIndex = 1;
+            this.FirstNameTextBox.TabIndex = 0;
             // 
             // label3
             // 
@@ -132,98 +127,45 @@
             // SaveButton
             // 
             this.SaveButton.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SaveButton.Location = new System.Drawing.Point(581, 329);
+            this.SaveButton.Location = new System.Drawing.Point(544, 325);
             this.SaveButton.Name = "SaveButton";
-            this.SaveButton.Size = new System.Drawing.Size(30, 23);
-            this.SaveButton.TabIndex = 2;
-            this.SaveButton.Text = "+";
+            this.SaveButton.Size = new System.Drawing.Size(67, 23);
+            this.SaveButton.TabIndex = 0;
+            this.SaveButton.Text = "ذخیره";
             this.SaveButton.UseVisualStyleBackColor = true;
             this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
             // 
-            // NewButton
+            // UpdateButton
             // 
-            this.NewButton.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NewButton.Location = new System.Drawing.Point(550, 329);
-            this.NewButton.Name = "NewButton";
-            this.NewButton.Size = new System.Drawing.Size(30, 23);
-            this.NewButton.TabIndex = 2;
-            this.NewButton.Text = "*";
-            this.NewButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpdateButton.Location = new System.Drawing.Point(477, 325);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(67, 23);
+            this.UpdateButton.TabIndex = 1;
+            this.UpdateButton.Text = "بروز رسانی";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
             // 
             // DeleteButton
             // 
             this.DeleteButton.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DeleteButton.Location = new System.Drawing.Point(519, 329);
+            this.DeleteButton.Location = new System.Drawing.Point(410, 325);
             this.DeleteButton.Name = "DeleteButton";
-            this.DeleteButton.Size = new System.Drawing.Size(30, 23);
+            this.DeleteButton.Size = new System.Drawing.Size(67, 23);
             this.DeleteButton.TabIndex = 2;
-            this.DeleteButton.Text = "-";
+            this.DeleteButton.Text = "حذف";
             this.DeleteButton.UseVisualStyleBackColor = true;
             this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
-            // 
-            // LastRecordButton
-            // 
-            this.LastRecordButton.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LastRecordButton.Location = new System.Drawing.Point(488, 329);
-            this.LastRecordButton.Name = "LastRecordButton";
-            this.LastRecordButton.Size = new System.Drawing.Size(30, 23);
-            this.LastRecordButton.TabIndex = 2;
-            this.LastRecordButton.Text = ">|";
-            this.LastRecordButton.UseVisualStyleBackColor = true;
-            // 
-            // NextRecordButton
-            // 
-            this.NextRecordButton.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NextRecordButton.Location = new System.Drawing.Point(457, 329);
-            this.NextRecordButton.Name = "NextRecordButton";
-            this.NextRecordButton.Size = new System.Drawing.Size(30, 23);
-            this.NextRecordButton.TabIndex = 2;
-            this.NextRecordButton.Text = ">";
-            this.NextRecordButton.UseVisualStyleBackColor = true;
-            // 
-            // PreviousRecordButton
-            // 
-            this.PreviousRecordButton.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PreviousRecordButton.Location = new System.Drawing.Point(426, 329);
-            this.PreviousRecordButton.Name = "PreviousRecordButton";
-            this.PreviousRecordButton.Size = new System.Drawing.Size(30, 23);
-            this.PreviousRecordButton.TabIndex = 2;
-            this.PreviousRecordButton.Text = "<";
-            this.PreviousRecordButton.UseVisualStyleBackColor = true;
-            // 
-            // FirstRecordButton
-            // 
-            this.FirstRecordButton.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FirstRecordButton.Location = new System.Drawing.Point(395, 329);
-            this.FirstRecordButton.Name = "FirstRecordButton";
-            this.FirstRecordButton.Size = new System.Drawing.Size(30, 23);
-            this.FirstRecordButton.TabIndex = 2;
-            this.FirstRecordButton.Text = "|<";
-            this.FirstRecordButton.UseVisualStyleBackColor = true;
             // 
             // CriteriaTextBox
             // 
             this.CriteriaTextBox.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CriteriaTextBox.Location = new System.Drawing.Point(221, 331);
+            this.CriteriaTextBox.Location = new System.Drawing.Point(234, 327);
             this.CriteriaTextBox.Name = "CriteriaTextBox";
             this.CriteriaTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.CriteriaTextBox.Size = new System.Drawing.Size(170, 21);
-            this.CriteriaTextBox.TabIndex = 1;
-            // 
-            // FieldToSearchComboBox
-            // 
-            this.FieldToSearchComboBox.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FieldToSearchComboBox.FormattingEnabled = true;
-            this.FieldToSearchComboBox.Items.AddRange(new object[] {
-            "نام",
-            "نام خانوادگی",
-            "شماره موبایل"});
-            this.FieldToSearchComboBox.Location = new System.Drawing.Point(94, 329);
-            this.FieldToSearchComboBox.Name = "FieldToSearchComboBox";
-            this.FieldToSearchComboBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.FieldToSearchComboBox.Size = new System.Drawing.Size(121, 21);
-            this.FieldToSearchComboBox.TabIndex = 3;
-            this.FieldToSearchComboBox.Text = "انتخاب کنید";
+            this.CriteriaTextBox.TabIndex = 7;
+            this.CriteriaTextBox.TextChanged += new System.EventHandler(this.CriteriaTextBox_TextChanged);
             // 
             // ContactsDataView
             // 
@@ -236,7 +178,9 @@
             this.ContactsDataView.ReadOnly = true;
             this.ContactsDataView.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.ContactsDataView.Size = new System.Drawing.Size(599, 164);
-            this.ContactsDataView.TabIndex = 4;
+            this.ContactsDataView.TabIndex = 9;
+            this.ContactsDataView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ContactsDataView_CellClick);
+            this.ContactsDataView.Click += new System.EventHandler(this.ContactsDataView_Click);
             // 
             // tblContactBindingSource
             // 
@@ -248,21 +192,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(626, 360);
             this.Controls.Add(this.ContactsDataView);
-            this.Controls.Add(this.FieldToSearchComboBox);
-            this.Controls.Add(this.FirstRecordButton);
-            this.Controls.Add(this.PreviousRecordButton);
             this.Controls.Add(this.CriteriaTextBox);
-            this.Controls.Add(this.NextRecordButton);
-            this.Controls.Add(this.LastRecordButton);
             this.Controls.Add(this.DeleteButton);
-            this.Controls.Add(this.NewButton);
+            this.Controls.Add(this.UpdateButton);
             this.Controls.Add(this.SaveButton);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ContactItemsGoupBox);
             this.Name = "Main";
             this.Text = "Contacts";
             this.Load += new System.EventHandler(this.Main_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.ContactItemsGoupBox.ResumeLayout(false);
+            this.ContactItemsGoupBox.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ContactsDataView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tblContactBindingSource)).EndInit();
             this.ResumeLayout(false);
@@ -272,7 +211,7 @@
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox ContactItemsGoupBox;
         private System.Windows.Forms.TextBox MobileTextBox;
         private System.Windows.Forms.TextBox LastNameTextBox;
         private System.Windows.Forms.TextBox FirstNameTextBox;
@@ -280,14 +219,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button SaveButton;
-        private System.Windows.Forms.Button NewButton;
+        private System.Windows.Forms.Button UpdateButton;
         private System.Windows.Forms.Button DeleteButton;
-        private System.Windows.Forms.Button LastRecordButton;
-        private System.Windows.Forms.Button NextRecordButton;
-        private System.Windows.Forms.Button PreviousRecordButton;
-        private System.Windows.Forms.Button FirstRecordButton;
         private System.Windows.Forms.TextBox CriteriaTextBox;
-        private System.Windows.Forms.ComboBox FieldToSearchComboBox;
         private System.Windows.Forms.DataGridView ContactsDataView;
         private System.Windows.Forms.BindingSource tblContactBindingSource;
     }
--- Contacts/Main.cs
+++ Contacts/Main.cs
@@ -30,8 +30,18 @@ namespace Contacts
                 DB.tblContacts.Add(Contact);
                 DB.SaveChanges();
                 ContactsDataView.DataSource = DB.tblContacts.ToList();
+                ClearFormItems();
             }
 
+        }
+
+        private void ClearFormItems()
+        {
+            foreach (Control control in ContactItemsGoupBox.Controls)
+            {
+                if (control is TextBox) control.Text = String.Empty;
+            }
+            //FirstNameTextBox.Text = LastNameTextBox.Text = MobileTextBox.Text = string.Empty;
         }
 
         private void Main_Load(object sender, EventArgs e)
@@ -40,6 +50,12 @@ namespace Contacts
             {
                 ContactsDataView.DataSource = DB.tblContacts.ToList();
             }
+            ContactsDataView.Columns[0].HeaderText = "نام";
+            ContactsDataView.Columns[1].HeaderText = "نام خانوادگی";
+            ContactsDataView.Columns[2].HeaderText = "موبایل";
+
+            ToolTip toolTip = new ToolTip();
+            toolTip.SetToolTip(CriteriaTextBox, "عبارت مورد نظر برای جستجو را وارد کنید");
         }
 
         private void DeleteButton_Click(object sender, EventArgs e)
@@ -51,7 +67,79 @@ namespace Contacts
                 DB.tblContacts.Remove(Contact);
                 DB.SaveChanges();
                 ContactsDataView.DataSource = DB.tblContacts.ToList();
+                ClearFormItems();
             }
+        }
+
+        //private void LastRecordButton_Click(object sender, EventArgs e)
+        //{
+        //    using (var DB = new ContactsDBEntities())
+        //    {
+        //        var Contact = DB.tblContacts.ToList().Last();
+        //        FirstNameTextBox.Text = Contact.FisrtName;
+        //        LastNameTextBox.Text = Contact.LastName;
+        //        MobileTextBox.Text = Contact.Mobile;
+        //    }
+        //}
+
+        //private void FirstRecordButton_Click(object sender, EventArgs e)
+        //{
+        //    using (var DB = new ContactsDBEntities())
+        //    {
+        //        var Contact = DB.tblContacts.ToList().First();
+        //        FirstNameTextBox.Text = Contact.FisrtName;
+        //        LastNameTextBox.Text = Contact.LastName;
+        //        MobileTextBox.Text = Contact.Mobile;
+        //    }
+        //}
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            if (MobileTextBox.Text.Trim() != String.Empty)
+                using (var DB = new ContactsDBEntities())
+                {
+                    var Contact = DB.tblContacts.Where(c => c.Mobile == MobileTextBox.Text.Trim()).Single();
+                    Contact.FisrtName = FirstNameTextBox.Text;
+                    Contact.LastName = LastNameTextBox.Text;
+                    Contact.Mobile = MobileTextBox.Text;
+                    DB.SaveChanges();
+                    ContactsDataView.DataSource = DB.tblContacts.ToList();
+                }
+        }
+
+        private void CriteriaTextBox_TextChanged(object sender, EventArgs e)
+        {
+            using (var DB = new ContactsDBEntities())
+            {
+                if (CriteriaTextBox.Text.Trim() != string.Empty)
+                {
+                    var Contact = DB.tblContacts.Where(
+                        c => c.Mobile.Contains(CriteriaTextBox.Text.Trim()) ||
+                        c.FisrtName.Contains(CriteriaTextBox.Text.Trim()) ||
+                        c.LastName.Contains(CriteriaTextBox.Text.Trim())).ToList();
+
+                    ContactsDataView.DataSource = Contact.ToList();
+                }
+                else
+                    ContactsDataView.DataSource = DB.tblContacts.ToList();
+            }
+        }
+
+        private void ContactsDataView_Click(object sender, EventArgs e)
+        {
+            //string key = ContactsDataView.SelectedRows[0].Cells[2].Value.ToString();
+
+        }
+
+        private void ContactsDataView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                FirstNameTextBox.Text = ContactsDataView.Rows[e.RowIndex].Cells[0].Value.ToString();
+                LastNameTextBox.Text = ContactsDataView.Rows[e.RowIndex].Cells[1].Value.ToString();
+                MobileTextBox.Text = ContactsDataView.Rows[e.RowIndex].Cells[2].Value.ToString();
+            }
+            catch { }
         }
     }
 }
